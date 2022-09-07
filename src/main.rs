4ab7cda8be7651b74c3ab0a0368b67ed0ae6dae5
lonnonjamesd <lemonjameslem@protonmail.com>
2022-09-07 02:17:22 -0400

    Added Args Struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use clap::Parser;
+
+/// Args to the program
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    #[clap(short, long)]
+    file: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
