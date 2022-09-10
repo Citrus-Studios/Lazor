2e66b400256722af2f783d5670c1b9a29412b3a0
lonnonjamesd <lemonjameslem@protonmail.com>
2022-09-10 16:06:40 -0400

    Added `prepare` function

--- src/terminal_management/mod.rs
+++ src/terminal_management/mod.rs
@@ -1,3 +1,12 @@
+mod terminal_manager;
+
+use std::io::stdout;
+
+use crossterm::{
+    execute,
+    terminal::{enable_raw_mode, Clear, ClearType},
+};
+
 /// This struct is used for managing the terminal
 pub struct TerminalManager {}
 
@@ -6,7 +15,14 @@ impl TerminalManager {
         Self {}
     }
 
-    pub fn run(self, function: fn() -> anyhow::Result<()>) {
+    // Prepares the terminal
+    fn prepare(&mut self) {
+        // enter raw mode and clear the screen and moves cursor to 0, 0
+        enable_raw_mode();
+        execute!(stdout(), Clear(ClearType::All), Cursor::MoveTo(0, 0))
+    }
+
+    pub fn run(&mut self, function: fn() -> anyhow::Result<()>) {
         loop {
             function().unwrap();
         }
