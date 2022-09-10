b8d95151bac9ba99bee08f634497525ceabecd44
lonnonjamesd <lemonjameslem@protonmail.com>
2022-09-10 16:13:05 -0400

    Fixed Errors

--- src/terminal_management/mod.rs
+++ src/terminal_management/mod.rs
@@ -1,9 +1,7 @@
-mod terminal_manager;
-
 use std::io::stdout;
 
 use crossterm::{
-    execute,
+    cursor, execute,
     terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType},
 };
 
@@ -19,7 +17,7 @@ impl TerminalManager {
     fn prepare(&mut self) {
         // enter raw mode and clear the screen and moves cursor to 0, 0
         enable_raw_mode();
-        execute!(stdout(), Clear(ClearType::All), Cursor::MoveTo(0, 0))
+        execute!(stdout(), Clear(ClearType::All), cursor::MoveTo(0, 0)).unwrap();
     }
 
     // Runs the event loop for the terminal
